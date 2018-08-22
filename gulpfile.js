c3425babbbc1d7a9b128e59d132e49a4f1ad8e7d
dannybeast <dannybeastrus@gmail.com>
2018-08-22 23:25:31 +0300

    привет

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+var syntax        = 'scss'; // Syntax: sass or scss;
+
+var gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		browsersync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require("gulp-notify"),
+		rsync         = require('gulp-rsync'),
+		pug           = require('gulp-pug');
+	
+gulp.task('pug', function buildHTML() {
+	return gulp.src('pug/pages/*.pug')
+	.pipe(pug({
+	  pretty:true
+	}))
+	.pipe(gulp.dest('app'))
+});
+
+gulp.task('browser-sync', function() {
+	browsersync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// open: false,
+		// tunnel: true,
+		// tunnel: "projectname", //Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browsersync.reload( {stream: true} ))
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/js/inputmask.js',
+		'app/js/jquery.inputmask.js',
+		'app/js/selectize.js',
+		'app/js/jquery-ui.min.js',
+		'app/libs/fancy/jquery.fancybox.min.js',
+		'app/js/jquery.custom-scrollbar.js',
+		'app/libs/swiper/dist/js/swiper.min.js',
+		// 'app/js/common.js', // Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify()) // Mifify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browsersync.reload({ stream: true }))
+});
+
+gulp.task('rsync', function() {
+	return gulp.src('app/**')
+	.pipe(rsync({
+		root: 'app/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'], // Includes files to deploy
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+gulp.task('watch', ['pug','styles', 'js', 'browser-sync'], function() {
+	gulp.watch('pug/**/*.pug', ['pug']);
+	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('app/*.html', browsersync.reload)
+});
+
+gulp.task('default', ['watch']);
